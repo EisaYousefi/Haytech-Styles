cc64a8eab21df9a4b7d577529cc1743bc180f53c
hayakom <yousefi.eisa.2910@gmail.com>
2020-06-29 17:04:33 +0430

    13990409 17:04

--- app/src/main/java/com/haytech/haytechstyle/MainActivity.java
+++ app/src/main/java/com/haytech/haytechstyle/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -13,13 +14,15 @@ import com.haytech.haytechstyle.databinding.ActivityMainBinding;
 import com.haytech.haytechstyles.Validation;
 import com.haytech.haytechstyles.editTextVerify.VerifyCodeClickListener;
 import com.haytech.haytechstyles.expandableLayout.DataList;
+import com.haytech.haytechstyles.utils.ThreadUtils;
+import com.haytech.haytechstyles.utils.Utils;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements Validation.phoneValidator {
 
     private ActivityMainBinding binding;
-
+    private ValueAnimator animator;
 
 
     @Override
@@ -62,12 +65,29 @@ public class MainActivity extends AppCompatActivity implements Validation.phoneV
                 Toast.makeText(this, "user and pass valid", Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
+        
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        simulateProgress();
+    }
+
+    private void simulateProgress() {
+        animator = ValueAnimator.ofInt(0, 100);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int progress = (int) animation.getAnimatedValue();
+                binding.circleProgressBar.setProgress(progress);
+            }
+        });
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setDuration(10000);
+        animator.start();
+    }
 
     @Override
     public void startPhoneNumber(String startPhoneNumber) {
