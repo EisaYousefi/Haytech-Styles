f03acb212fff736f3664215f4842b6cfe686f388
hayakom <yousefi.eisa.2910@gmail.com>
2020-06-30 08:44:55 +0430

    13990410 08:45

--- haytechstyles/src/main/java/com/haytech/haytechstyles/expandableLayout/DataList.java
+++ haytechstyles/src/main/java/com/haytech/haytechstyles/expandableLayout/DataList.java
@@ -62,11 +62,11 @@ public class DataList {
         lists.add(new DataList("مجید حسنی", "1398 / 11201/05 / 1008 / 244", false));
         lists.add(new DataList("وریا مرادی", "1398 / 11201/05 / 1008 / 244", false));
         lists.add(new DataList("بروا یوسفی", "1398 / 11201/05 / 1008 / 244", false));
-        lists.add(new DataList("عیسی یوسفی", "1398 / 11201/05 / 1008 / 244", false));
-        lists.add(new DataList("مهندس جانباز", "1398 / 11201/05 / 1008 / 244", false));
-        lists.add(new DataList("مجید حسنی", "1398 / 11201/05 / 1008 / 244", false));
-        lists.add(new DataList("وریا مرادی", "1398 / 11201/05 / 1008 / 244", false));
-        lists.add(new DataList("بروا یوسفی", "1398 / 11201/05 / 1008 / 244", false));
+        lists.add(new DataList(" یوسفی", "1398 / 11201/05 / 1008 / 244", false));
+        lists.add(new DataList(" جانباز", "1398 / 11201/05 / 1008 / 244", false));
+        lists.add(new DataList(" حسنی", "1398 / 11201/05 / 1008 / 244", false));
+        lists.add(new DataList(" مرادی", "1398 / 11201/05 / 1008 / 244", false));
+        lists.add(new DataList(" یوسفی", "1398 / 11201/05 / 1008 / 244", false));
         return lists;
     }
 }
